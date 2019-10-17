0981db1413dd7735d60e42547e43c634f9f27072
Jeff Swisher <jefftswisher@gmail.com>
2019-10-17 23:31:03 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Dog 
+
+  def name=(dog_name)
+    @dog_instance_name = dog_name
+  end 
+  
+  def name
+    @dog_instance_name
+  end 
+  
+  def bark
+    puts "woof!"
+  end 
+end 
+
+Fido = Dog.new 
+Fido.name = "Fido" 
+
+puts Fido.name 
+puts Fido.bark
